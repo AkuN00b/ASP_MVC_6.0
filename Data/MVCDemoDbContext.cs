169f3b61be9c22ab5ff29b751c050d5d8126196e
GERLANDO ARYAF GARIBALDI CORPUTTY <0320210027@polman.astra.ac.id>
2022-12-03 03:01:31 +0700

    v1.0

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCcrud6.Models.Domain;
+
+namespace MVCcrud6.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext()
+        {
+        }
+
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
